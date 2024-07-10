1ecac305e45dd0159e3f656a8404791e616da6b1
ulugbek101 <thedevu101@gmail.com>
2024-07-10 18:17:17 +0500

    First commit

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i621*6t54eo31j#u)4&1my4r1r9i0wczj*g=6wye%#-s!4v&j!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
